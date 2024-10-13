d2a71de51d4c4963ecfe20f34fe63fb85de7742c
Sparsh Chaudhary <work.sparsh01@gmail.com>
2024-10-13 22:23:30 +0200

    Stats+gui
    Dat GUI and Stats Panel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -17,7 +19,7 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-new OrbitControls(camera,renderer.domElement)
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -25,13 +27,30 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 20)
+cameraFolder.open()
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
+
+  stats.update()
 }
 
 animate()
